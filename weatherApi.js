20fbccc082622c0fbe344df5f01d31cb82adda9e
MujtabaMuhammad <34104990+MujtabaMuhammad@users.noreply.github.com>
2024-11-09 19:42:16 -0500

    modification to html file inclusion of APIKey requirement, formatting of the weather data display

--- weatherApi.js
+++ weatherApi.js
@@ -1,21 +1,22 @@
-
 document.getElementById('weatherForm').addEventListener('submit',weatherData);
 
 function weatherData(e){
     e.preventDefault();
     lat = document.getElementById('lat').value;
     long = document.getElementById('long').value;
-    console.log(lat,long)
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=e965e30f677c0258b5f34bceed2b9131`)
+    apid = document.getElementById('api-key').value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apid}&units=imperial`)
     .then(response =>{
         return response.json();})
         .then(data => {
-            console.log("Temperature parameters: ", data.main);
-            let recoveredData = document.createTextNode(`Temprature: ${data.main.temp}
-                Humidity: ${data.main.humidity}
+            //console.log("Temperature parameters: ", data.name);
+            let recoveredData = document.createTextNode(`Location: ${data.name}
+                Temprature: ${data.main.temp}°F
+                Humidity: ${data.main.humidity}%
                 Description: ${data.weather[0].description}`);
+                dataDiv.style.display = "block";
             document.getElementById('weather-result').appendChild(recoveredData);
         }).catch(error => {
                 console.log(error);
-                alert('An error occured, please check coordinates.');})
+                alert('An error occured, please check coordinates and verify your API key.');})
 }
